9eb358b63953119e1f29bdbfce2956fd12cd7dbc
langlangHi <286357108@qq.com>
2020-02-28 23:05:33 +0800

    项目初始化

---
+++ src/main/java/com/liang/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.liang.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello liang";
+    }
+}
